5287e54c18c05c254b660161a44e19296b210d77
Chris McHardy <chris.mchardy@bbd.co.za>
2025-06-01 11:36:54 +0200

    Update database design

--- flyway/V1__init.sql
+++
@@ -1,7 +0,0 @@
--- Users table
-CREATE TABLE users (
-    user_id SERIAL PRIMARY KEY,
-    email VARCHAR(255) NOT NULL UNIQUE,
-    username VARCHAR(255) NOT NULL UNIQUE,
-    created_at TIMESTAMP DEFAULT now()
-);
