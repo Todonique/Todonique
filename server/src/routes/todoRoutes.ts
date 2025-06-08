4c6ebb35ccaa3dd47267bd69854994835dcaf575
Chris McHardy <chris.mchardy@bbd.co.za>
2025-06-08 13:57:43 +0200

    Update to add todos by team, with ensuring user request is from a team member

--- server/src/routes/todoRoutes.ts
+++ server/src/routes/todoRoutes.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import { createTodoHandler, updateTodoHandler, getTodosByUserInTeamHandler } from '../controller';
 import { authorize } from '../middleware';
+import { getTodosByTeamHandler } from '../controller/todoController';
 
 const router = express.Router();
 
@@ -8,5 +9,6 @@ router.post('/todo', authorize('user'), createTodoHandler);
 router.patch('/todo', authorize('user'), updateTodoHandler);
 router.patch('/todo/:todoId', authorize('teamlead'), updateTodoHandler);
 router.get('/todo/:userId/team/:teamId', authorize('user'), getTodosByUserInTeamHandler);
+router.get('/todo/team/:teamId', authorize('user'), getTodosByTeamHandler);
 
 export default router;
